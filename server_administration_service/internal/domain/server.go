81f8c001bdb427dfa8302ad317b77722cda0dc01
flashhhhh <nguyenchithanhpro196@gmail.com>
2025-04-18 01:02:55 +0700

    feat: add server administration service layout

---
+++ server_administration_service/internal/domain/server.go
@@ -0,0 +1,12 @@
+package domain
+
+import "time"
+
+type Server struct {
+	ServerID string `json:"server_id" gorm:"primary_key"`
+	ServerName string `json:"server_name" gorm:"unique;not null"`
+	Status string `json:"status" gorm:"not null"`
+	CreatedTime time.Time `json:"created_time" gorm:"autoCreateTime"`
+	LastUpdated time.Time `json:"last_updated" gorm:"autoUpdateTime"`
+	IPAddress string `json:"ip_address" gorm:"not null"`
+}
