dc2d195d6a95c609e86b211d2f00c0b6ec4e1588
flashhhhh <nguyenchithanhpro196@gmail.com>
2025-04-21 01:02:13 +0700

    feat: Add grpc server for user service

--- user_service/infrastructure/grpc/main.go
+++ user_service/infrastructure/grpc/main.go
@@ -1,1 +1,26 @@
 package grpc
+
+import (
+	"log"
+	"net"
+	grpchandler "user_service/internal/grpc_handler"
+	"user_service/pb"
+
+	"google.golang.org/grpc"
+)
+
+func StartGRPCServer(userHandler *grpchandler.UserHandler, port string) {
+	lis, err := net.Listen("tcp", ":" + port)
+	if err != nil {
+		panic(err)
+	}
+
+	// Create a new gRPC server
+	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, userHandler)
+
+	log.Println("gRPC server is running on port: ", port)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
---
+++ user_service/internal/grpc_handler/user_handler.go
@@ -0,0 +1,104 @@
+package grpchandler
+
+import (
+	"context"
+	"user_service/internal/service"
+	"user_service/pb"
+
+	"github.com/flashhhhh/pkg/logging"
+)
+
+type UserHandler struct {
+	userService service.UserService
+	pb.UnimplementedUserServiceServer
+}
+
+func NewGrpcUserHandler(userService service.UserService) *UserHandler {
+	return &UserHandler{
+		userService: userService,
+	}
+}
+
+func (grpcHandler *UserHandler) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	logging.LogMessage("user_service", "Creating new user", "INFO")
+	logging.LogMessage("user_service", "username: "+req.Username+", password: "+req.Password+", name: "+req.Name+", email: "+req.Email+", role: "+req.Role, "DEBUG")
+
+	userID, err := grpcHandler.userService.CreateUser(ctx, req.Username, req.Password, req.Name, req.Email, req.Role)
+	if err != nil {
+		logging.LogMessage("user_service", "Failed to create user: "+err.Error(), "ERROR")
+		return nil, err
+	}
+
+	logging.LogMessage("user_service", "User created successfully with ID: "+userID, "INFO")
+	return &pb.CreateUserResponse{
+		UserID:  userID,
+	}, nil
+}
+
+func (grpcHandler *UserHandler) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
+	logging.LogMessage("user_service", "User login attempt", "INFO")
+	logging.LogMessage("user_service", "username: "+req.Username+", password: "+req.Password, "DEBUG")
+
+	token, err := grpcHandler.userService.Login(ctx, req.Username, req.Password)
+	if err != nil {
+		logging.LogMessage("user_service", "Failed to login user: "+err.Error(), "ERROR")
+		return nil, err
+	}
+
+	logging.LogMessage("user_service", "User logged in successfully", "INFO")
+	logging.LogMessage("user_service", "Generated token: "+token, "DEBUG")
+
+	return &pb.LoginResponse{
+		Token: token,
+	}, nil
+}
+
+func (grpcHandler *UserHandler) GetUserByID(ctx context.Context, req *pb.IDRequest) (*pb.UserResponse, error) {
+	logging.LogMessage("user_service", "Fetching user by ID", "INFO")
+	logging.LogMessage("user_service", "userID: "+req.Id, "DEBUG")
+
+	user, err := grpcHandler.userService.GetUserByID(ctx, req.Id)
+	if err != nil {
+		logging.LogMessage("user_service", "Failed to fetch user: "+err.Error(), "ERROR")
+		return nil, err
+	}
+
+	logging.LogMessage("user_service", "User fetched successfully", "INFO")
+	return &pb.UserResponse{
+		UserID: user.ID,
+		Username: user.Username,
+		Name:     user.Name,
+		Email:    user.Email,
+		Role:     user.Role,
+	}, nil
+}
+
+func (grpcHandler *UserHandler) GetAllUsers(ctx context.Context, req *pb.EmptyRequest) (*pb.UsersResponse, error) {
+	logging.LogMessage("user_service", "Fetching all users", "INFO")
+
+	users, err := grpcHandler.userService.GetAllUsers(ctx)
+	if err != nil {
+		logging.LogMessage("user_service", "Failed to fetch users: "+err.Error(), "ERROR")
+		return nil, err
+	}
+
+	logging.LogMessage("user_service", "All users fetched successfully", "INFO")
+
+	logging.LogMessage("user_service", "Converting users to protobuf form", "DEBUG")
+	var pbUsers []*pb.UserResponse
+	for _, user := range users {
+		pbUsers = append(pbUsers, &pb.UserResponse{
+			UserID:   user.ID,
+			Username: user.Username,
+			Name:     user.Name,
+			Email:    user.Email,
+			Role:     user.Role,
+		})
+	}
+
+	logging.LogMessage("user_service", "Successfully converting users to protobuf form", "DEBUG")
+
+	return &pb.UsersResponse{
+		Users: pbUsers,
+	}, nil
+}
