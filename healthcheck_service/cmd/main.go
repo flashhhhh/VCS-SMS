3c25f8455697dd6c18e8f40e08f4f2b45558784d
flashhhhh <nguyenchithanhpro196@gmail.com>
2025-05-18 18:22:31 +0700

    feat: add deployment environment to healthcheck service

--- healthcheck_service/cmd/main.go
+++ healthcheck_service/cmd/main.go
@@ -22,8 +22,22 @@ func main() {
 	serverServiceLogPath := filepath.Join(currentPath, "logs", "healthcheck_service.log")
 	logging.InitLogger("healthcheck_service", serverServiceLogPath, 10, 5, 30)
 
+	// Load running environment variable
+	environment := env.GetEnv("RUNNING_ENVIRONMENT", "local")
+	logging.LogMessage("healthcheck_service", "Running in "+environment+" environment", "INFO")
+
+	// Load environment variables from the .env file
+	environmentFilePath := filepath.Join(currentPath, "configs", environment+".env")
+	if err := env.LoadEnv(environmentFilePath); err != nil {
+		logging.LogMessage("healthcheck_service", "Failed to load environment variables from "+environmentFilePath+": "+err.Error(), "FATAL")
+		logging.LogMessage("healthcheck_service", "Exiting the program...", "FATAL")
+		os.Exit(1)
+	} else {
+		logging.LogMessage("healthcheck_service", "Environment variables loaded successfully from "+environmentFilePath, "INFO")
+	}
+
 	// Initialize Kafka Producer
-	kafkaProducer, err := kafka.NewKafkaProducer([]string{env.GetEnv("KAFKA_ADDRESS", "localhost:9092")})
+	kafkaProducer, err := kafka.NewKafkaProducer([]string{env.GetEnv("KAFKA_HOST", "localhost") + ":" + env.GetEnv("KAFKA_PORT", "9092")})
 	if err != nil {
 		panic(err)
 	}
--- healthcheck_service/infrastructure/grpc/main.go
+++ healthcheck_service/infrastructure/grpc/main.go
@@ -3,12 +3,13 @@ package grpc
 import (
 	"healthcheck_service/pb"
 
+	"github.com/flashhhhh/pkg/env"
 	"google.golang.org/grpc"
 )
 
 func StartGRPCClient() (pb.ServerAdministrationServiceClient, error) {
 	// Create a connection to the server.
-	conn, err := grpc.Dial("localhost:50052", grpc.WithInsecure())
+	conn, err := grpc.Dial(env.GetEnv("GRPC_SERVER_ADMINISTRATION_SERVER", "localhost") + ":" + env.GetEnv("GRPC_SERVER_ADMINISTRATION_PORT", "50052"), grpc.WithInsecure())
 
 	// Create a new client
 	client := pb.NewServerAdministrationServiceClient(conn)
