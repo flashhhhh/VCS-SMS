b3e4fa245e66c0207cebe334f557c3e29df2dc99
flashhhhh <nguyenchithanhpro196@gmail.com>
2025-05-20 01:03:36 +0700

    feat: add goroutine in receive message in Kafka

--- server_administration_service/internal/handler/kafka_handler.go
+++ server_administration_service/internal/handler/kafka_handler.go
@@ -28,37 +28,39 @@ func (h ServerConsumerHandler) Cleanup(sarama.ConsumerGroupSession) error {
 
 func (h ServerConsumerHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for message := range claim.Messages() {
-		logging.LogMessage("server_administration_service", "Received message: " + string(message.Value), "INFO")
-		session.MarkMessage(message, "")
+		go func(message *sarama.ConsumerMessage) {
+			logging.LogMessage("server_administration_service", "Received message: " + string(message.Value), "INFO")
+			session.MarkMessage(message, "")
 
-		// Parse the message
-		var serverMessage struct {
-			IPv4     string `json:"ipv4"`
-			ID int `json:"id"`
-			Status   bool   `json:"status"`
-		}
-		
-		if err := json.Unmarshal(message.Value, &serverMessage); err != nil {
-			logging.LogMessage("server_administration_service", "Error parsing message: "+err.Error(), "ERROR")
-			continue
-		}
-		
-		// Now you can use the parsed message
-		logging.LogMessage("server_administration_service", "Updating server status: "+serverMessage.IPv4, "INFO")
+			// Parse the message
+			var serverMessage struct {
+				IPv4     string `json:"ipv4"`
+				ID int `json:"id"`
+				Status   bool   `json:"status"`
+			}
+			
+			if err := json.Unmarshal(message.Value, &serverMessage); err != nil {
+				logging.LogMessage("server_administration_service", "Error parsing message: "+err.Error(), "ERROR")
+				return
+			}
+			
+			// Now you can use the parsed message
+			logging.LogMessage("server_administration_service", "Updating server status: "+serverMessage.IPv4, "INFO")
 
-		status := "Off"
-		if serverMessage.Status {
-			status = "On"
-		}
-		h.serverService.UpdateServerStatus(serverMessage.ID, status)
+			status := "Off"
+			if serverMessage.Status {
+				status = "On"
+			}
+			h.serverService.UpdateServerStatus(serverMessage.ID, status)
 
-		logging.LogMessage("server_administration_service", "Write to ES: "+serverMessage.IPv4, "INFO")
-		err := h.serverService.AddServerStatus(serverMessage.ID, status)
-		if err != nil {
-			logging.LogMessage("server_administration_service", "Error writing to ES: "+err.Error(), "ERROR")
-		}
+			logging.LogMessage("server_administration_service", "Write to ES: "+serverMessage.IPv4, "INFO")
+			err := h.serverService.AddServerStatus(serverMessage.ID, status)
+			if err != nil {
+				logging.LogMessage("server_administration_service", "Error writing to ES: "+err.Error(), "ERROR")
+			}
 
-		logging.LogMessage("server_administration_service", "Message processed: " + string(message.Value), "INFO")
+			logging.LogMessage("server_administration_service", "Message processed: " + string(message.Value), "INFO")
+		}(message)
 	}
 
 	return nil
